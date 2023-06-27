5360b2887e97ccbf00b1c015073bf0565ea0ddb5
Alex Vega <alexvega@Alexs-MBP.lan>
2023-06-27 18:18:56 -0400

    this adds the get and post api routes for the category-routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -16,13 +16,33 @@ router.get("/", async (req, res) => {
   }
 });
 
-router.get("/:id", (req, res) => {
+router.get("/:id", async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  try {
+    const categoriesData = await Category.findByPk(req.params.id, {
+      include: [{ model: Product }],
+    });
+    if (!categoriesData) {
+      res.status(404).json({ message: "No category found with that id!" });
+      return;
+    }
+    res.status(200).json(categoriesData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.post("/", (req, res) => {
+router.post("/", async (req, res) => {
   // create a new category
+  try {
+    const categoriesData = await Category.create({
+      category_name: req.body.category_name,
+    });
+    res.status(200).json(categoriesData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 router.put("/:id", (req, res) => {
