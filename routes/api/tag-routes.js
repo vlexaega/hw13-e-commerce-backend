8e00b4a8c81b733ba8b314470d528c02066c0f27
Alex Vega <alexvega@Alexs-MBP.lan>
2023-06-27 19:13:17 -0400

    this adds the delete api route for tags

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -47,12 +47,44 @@ router.post("/", (req, res) => {
     });
 });
 
-router.put("/:id", (req, res) => {
+router.put("/:id", async (req, res) => {
   // update a tag's name by its `id` value
+  const tagId = req.params.id;
+  const updatedTagName = req.body.tag_name;
+
+  try {
+    const tag = await Tag.findByPk(tagId);
+
+    if (!tag) {
+      return res.status(404).json({ error: "Tag is not found" });
+    }
+    tag.tag_name = updatedTagName;
+    const updatedTag = await tag.save();
+    res.json(updatedTag);
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({ error: "Failed to update Tag" });
+  }
 });
 
-router.delete("/:id", (req, res) => {
+router.delete("/:id", async (req, res) => {
   // delete on tag by its `id` value
+  try {
+    const tagData = await Tag.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!tagData) {
+      res.status(404).json({ message: "No tag found with that id!" });
+      return;
+    }
+
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
